0634eb95fdd6ae81e57f1af1a9599a9878207984
rodlobcarlos <rodlobcarlos@gmail.com>
2025-10-06 21:13:16 +0200

    lanzador sin terminar

--- JAVA/simulacion_examen/src/main/java/boletin1/Lanzador.java
+++ JAVA/simulacion_examen/src/main/java/boletin1/Lanzador.java
@@ -4,22 +4,26 @@ import java.io.IOException;
 
 public class Lanzador {
 	
-	private static final String diractorioGenerarClasses = "C:\\Users\\alumno\\Desktop\\segundoDAM\\JAVA\\simulacion_examen\\target";
-	private static final String rutaFicherosJava = "C:\\Users\\alumno\\Desktop\\segundoDAM\\JAVA\\simulacion_examen\\src\\main\\java";
+	private static final String diractorioGenerarClasses = "C:\\Users\\rodlo\\Documents\\GitHub\\segundoDAM\\segundoDAM\\JAVA\\simulacion_examen\\target";
+	private static final String rutaFicherosJava = "C:\\Users\\rodlo\\Documents\\GitHub\\segundoDAM\\segundoDAM\\JAVA\\simulacion_examen\\src\\main\\java";
 	
 	public static void main(String[] args) {
-		
 		
 		
 	}
 	
 	public void ejecutarProceso() {
-		String[] comando = {"javac","-d", diractorioGenerarClasses, rutaFicherosJava + "\\boletin1\\Gestiona.java"};
-		ProcessBuilder pb = new ProcessBuilder(comando);
-		
 		try {
+			String[] comando = {"javac","-d", diractorioGenerarClasses, rutaFicherosJava + "boletin1\\Gestiona.java"};
+			ProcessBuilder pb = new ProcessBuilder(comando);
+			Process p = pb.start();
+			pb.inheritIO();
 			Process proceso = pb.start();
+			int exit = proceso.waitFor();
 		}catch (IOException e) {
+			e.printStackTrace();
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
 	}
