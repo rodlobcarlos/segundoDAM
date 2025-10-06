e60e9105516ba414163a37a2024f29afeb40e4e6
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-10-06 09:04:11 +0200

    js DOM

--- Optativa/js/myscript.js
+++ Optativa/js/myscript.js
@@ -1,4 +1,3 @@
-/*
 console.log("Hola buenos dias!");
 
 var table = "Normal Table";
@@ -70,28 +69,28 @@ var array = new Array(3,4,6,7,8,9,10,3,2,1,3,5,4,9,78,45,32,12,3,65);
 for(var i= 0; array.length > i; i++){
     console.log(array[i]);
 }
-*/
+
 var array = new Array(3,4,6,7,8,9,10,3,2,1,3,5,4,9,78,45,32,12,3,65);
-/*
+
 for(var i = third_array.length-1; i>=0 ;i--) {
     console.log("Entramos en la iteración de " + array);
 }
-*/
 
-/*
+
+
 var i = array.length-1;
 for(; i>=0; i--){
     console.log("Entramos en la iteración de " +array[i]);
 }
-*/
 
-/*
+
+
 array.forEach(function myFunction(item) {
   console.log(item); 
 });
-*/
 
-/*
+
+
 // date object
 let today = new Date();
 let firstOctober = new Date(2025, 9, 1);
@@ -104,8 +103,32 @@ if(today>firstOctober) {
 } else {
     console.log("Is before");
 }
-*/
 
 function myFirstFuntion() {
     console.log("Thank you for your click");
 }
+
+function mySecondFuntion() {
+    console.log("Thank you for your interest");
+}
+
+function myThirdFuntion() {
+    console.log("Pesado");
+}
+
+// Select DOM
+var div = document.getElementById("my_div");
+div.classList.add("my_class");
+console.log(div);
+
+var div2 = document.getElementsByTagName("div");
+console.log(div2);
+
+var div3 = document.querySelector("#my_second_div");
+console.log(my_second_div);
+
+function $(selector) {
+    return document.querySelectorAll(selector);
+}
+console.log($("#my_third_div"));
+console.log($("#my_third_div".classList));
