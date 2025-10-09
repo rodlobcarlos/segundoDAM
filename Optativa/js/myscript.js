9427b72bfdd93961e1c24b2f0c7ee4b867d44d2e
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-10-09 14:06:44 +0200

    js funciones, myAlert, bind_close

--- Optativa/js/myscript.js
+++ Optativa/js/myscript.js
@@ -158,3 +158,27 @@ $("#btn").addEventListener("click", function(){
     $("#form").appendChild(input);
     myAlert("Add new email input");
 });
+
+function myAlert(msg) {
+    var div = document.createElement("div");
+    div.classList.add("alert");
+    div.innerHTML = msg;
+    var close = document.createElement("span");
+    close.style.float = "right";
+    close.classList.add("close");
+    close.innerHTML = "X"
+    div.appendChild(close);
+    $("body").insertBefore(div,$("body").firstChild);
+    console.log($("body").children[1]);
+    bind_close();
+}
+
+function bind_close() {
+    let elements = document.querySelectorAll(".close");
+    for(var i=elements.length-1;i>=0;i--) {
+        let el = elements[i];
+        el.addEventListener("click", function() {
+            this.parentNode.style.display = "none";
+        });
+    }
+}
