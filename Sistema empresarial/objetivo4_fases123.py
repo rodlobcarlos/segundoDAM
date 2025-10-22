20a68c69eadc54847994344c1653c8fdf303384b
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-10-22 10:11:58 +0200

    cambios

--- Sistema empresarial/objetivo4_fases123.py
+++ Sistema empresarial/objetivo4_fases123.py
@@ -1,6 +1,10 @@
 # Carlos Rodríguez Lobato / Grupo 2ºDAM
 
 alumno = int(input("¿Cuantos alumnos hay?: "))
+suma_media = 0
+contador1 = 0
+contador2 = 0
+contador3 = 0
 
 if alumno <= 0:
     print("No puede haber 0 alumnos o menos!")
@@ -13,11 +17,20 @@ else:
             for i in range(total_notas):
                 nota = int(input("Escribe la nota: "))
                 media = (nota + nota) / total_notas
+                suma_media +=media
             if media >= 5:
                 print("Aprobado")
+                contador1 +=1
             elif media > 4 and media < 5:
                 print("Necesita mejorar")
+                contador2 +=1
             else:
                 print("Suspenso")
-            print(media)    
+                contador3 +=1
+            print(media)
         contador += 1
+suma_media = suma_media / alumno
+print("Media total: ", suma_media)
+print("Aprobados", contador1)
+print("Necesita mejorar", contador2)
+print("Suspenso", contador3)
