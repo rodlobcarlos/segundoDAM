85604050f36a553b54d5d93f5926fe072cbf5637
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-10-22 09:44:22 +0200

    alumnos

--- Sistema empresarial/objetivo4_fases123.py
+++ Sistema empresarial/objetivo4_fases123.py
@@ -2,24 +2,22 @@
 
 alumno = int(input("¿Cuantos alumnos hay?: "))
 
-while alumno != 0:
-    nombre = str(input("Escrie su nombre: "))
-    for nombre in range(alumno):
-        total_notas = int(input("¿Cuantas notas tiene?: "))
-        if total_notas <= 0:
-            print("Dato no válido")
-        else:
-            nota = int(input("Dame una nota: "))
-    for nota in range(total_notas):
-        media = (nota + nota) / total_notas
-        if media >= 5:
-            print("Apronado")
-        elif media > 4 and media < 5:
-            print("Necesita mejorar")
-        elif media < 4:
-            print("Suspenso")
-            
-        
-    
-if(alumno <= 0):
+if alumno <= 0:
     print("No puede haber 0 alumnos o menos!")
+else:
+    contador = 0
+    while contador < alumno:
+        nombre = input("Escribe un nombre: ")
+        total_notas = int(input("¿Cuantas notas tiene?: "))
+        if total_notas != 0:
+            for i in range(total_notas):
+                nota = int(input("Escribe la nota: "))
+                media = (nota + nota) / total_notas
+            if media >= 5:
+                print("Aprobado")
+            elif media > 4 and media < 5:
+                print("Necesita mejorar")
+            else:
+                print("Suspenso")
+            print(media)    
+        contador += 1
