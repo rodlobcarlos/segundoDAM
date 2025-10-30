6a63d7c7f2fc0da2e40255feb317460baeb804c8
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-10-30 10:12:22 +0100

    python notes

--- desarrolloInterfaces/notes/size2.0.py
+++ desarrolloInterfaces/notes/size2.0.py
@@ -1,9 +1,9 @@
 '''
-Este código muetra una ventana de tamaño fijo
+Este código muestra el tamaño máximo y mínimo de la ventana
 '''
 
 '''Importe de clases'''
-from PySide6.QtCore import QSize 
+from PySide6.QtCore import QSize
 from PySide6.QtWidgets import QApplication, QMainWindow, QPushButton
 
 class MainWindow(QMainWindow):
@@ -11,9 +11,11 @@ class MainWindow(QMainWindow):
         super().__init__()
         self.setWindowTitle("My app")
         button = QPushButton("Press me!")
-
-        self.setFixedSize(QSize(400, 300)) # Tamaño fijo de la ventana
         self.setCentralWidget(button)
+
+        self.setMinimumSize(QSize(400, 200)) # Tamaño mínimo de la ventana
+        self.setMaximumSize(QSize(700, 400)) # Tamaño máximo de la ventana
+
 
 app = QApplication([])
 window = MainWindow()
