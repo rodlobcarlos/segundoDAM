361c34a56abda22bfe47c25dd5e56695a05082ba
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2025-11-17 09:02:51 +0100

    indexEJS

--- Optativa/03Express/01-express.js
+++ Optativa/03Express/01-express.js
@@ -1,11 +1,19 @@
+const { render } = require('ejs');
 const express = require('express')
 const app = express()
 const port = 3000
 
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+app.set('views', __dirname + '/views');
+app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
     res.send('Ya somos unos cracks en NodeExpress')
+})
+
+app.get('/pruebas', (req, res) => {
+    res.render("pruebas", {titulo: "Titulo dinamico"})
 })
 
 app.get('/contacto.html', (req, res) => {
@@ -14,6 +22,10 @@ app.get('/contacto.html', (req, res) => {
 
 app.get('/aboutUs.html', (req, res) => {
   res.send('aboutUs')
+})
+
+app.use((req, res) => {
+  res.status(404).sendFile(__dirname + "/public/html/404.html")
 })
 
 app.listen(port, () => {
