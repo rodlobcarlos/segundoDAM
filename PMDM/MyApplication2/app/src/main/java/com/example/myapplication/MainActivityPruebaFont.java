54653652081ae5eff411fdbb82472f58f57e6361
rodlobcarlos <rodlobcarlos@gmail.com>
2025-11-24 21:18:45 +0100

    u3.3

--- PMDM/MyApplication2/app/src/main/java/com/example/myapplication/MainActivityPruebaFont.java
+++ PMDM/MyApplication2/app/src/main/java/com/example/myapplication/MainActivityPruebaFont.java
@@ -3,24 +3,27 @@ package com.example.myapplication;
 import android.graphics.Typeface;
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
 import android.view.animation.Animation;
 import android.view.animation.AnimationUtils;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivityPruebaFont extends AppCompatActivity {
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_prueba_font);
-        Bundle b = new Bundle(getIntent().getExtras());
-        Log.i("Datos", b.getString("Surname"));
-        TextView miTexto1 = (TextView) findViewById(R.id.texto2);
-        miTexto1.setText(("Texto de la primera captura"));
-        Animation miAnimacion = AnimationUtils.loadAnimation(this,R.anim.animacion);
-        miAnimacion.setRepeatMode(Animation.RESTART);
-        miAnimacion.setRepeatCount(20);
-        miTexto1.startAnimation(miAnimacion);
-     }
+    protected void onCreate(Bundle savedIns) {
+        super.onCreate(savedIns);
+        setContentView(R.layout.activity_prueba_font);  // Llamada al XML
+
+        TextView tvMensaje = (TextView) findViewById(R.id.texto2);
+        tvMensaje.setText("Nuevo texto para mostrar");
+    }
+
+    // Método para el botón con android:onClick
+    public void cambiarTextoXML(View view) {
+        TextView tvMensaje = (TextView) findViewById(R.id.texto2);
+        tvMensaje.setText("Texto cambiado desde XML");
+    }
 }
