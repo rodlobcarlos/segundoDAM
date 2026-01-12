5e18e071a576072ae90c23f726e669faf4447572
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-12 08:47:41 +0100

    created main.ts

---
+++ Optativa/04_angular/main.ts
@@ -0,0 +1,9 @@
+// main.ts
+const saludos = (persona: string) => {
+    return "Hola, " + persona;
+}
+const usuario: string = "Marcos";
+console.log(usuario);
+document.body.innerHTML = saludos(usuario);
+
+export{};
