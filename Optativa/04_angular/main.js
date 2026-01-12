b34b5b6c3036bb6304454295828e34024025e37b
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-12 09:05:23 +0100

    main.ts changes

--- Optativa/04_angular/main.js
+++ Optativa/04_angular/main.js
@@ -1,7 +1,13 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
 // main.ts
 var saludos = function (persona) {
     return "Hola, " + persona;
 };
 var usuario = "Marcos";
 console.log(usuario);
+var sentencia = "Hola, mi nombre es ".concat(usuario);
+console.log(sentencia);
+//let object1: Array<number> = [a,1, b,2, c,3];
+//console.log(Object.values(object1))
 document.body.innerHTML = saludos(usuario);
--- Optativa/04_angular/main.ts
+++ Optativa/04_angular/main.ts
@@ -1,9 +1,15 @@
 // main.ts
-const saludos = (persona: string) => {
+let saludos = (persona: string) => {
     return "Hola, " + persona;
 }
-const usuario: string = "Marcos";
+let usuario: string = "Marcos";
 console.log(usuario);
+
+let sentencia = `Hola, mi nombre es ${usuario}`;
+console.log(sentencia);
+
+//let object1: Array<number> = [a,1, b,2, c,3];
+//console.log(Object.values(object1))
 document.body.innerHTML = saludos(usuario);
 
 export{};
