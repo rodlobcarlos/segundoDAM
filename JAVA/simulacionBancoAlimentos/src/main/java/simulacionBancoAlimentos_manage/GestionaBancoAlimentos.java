1eb70116782bda37e6b5fd6620706874d8e7f2b8
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-13 22:19:20 +0100

    Improve comments and logging in GestionaBancoAlimentos
    Updated comments for clarity and added explanations for variables and method calls.

--- JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_manage/GestionaBancoAlimentos.java
+++ JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_manage/GestionaBancoAlimentos.java
@@ -1,5 +1,6 @@
 package simulacionBancoAlimentos_manage;
 
+// --- Imports ---
 import java.util.List;
 
 import org.apache.logging.log4j.LogManager;
@@ -9,22 +10,27 @@ import simulacionBancoAlimentos_model.CentroLogistico;
 import simulacionBancoAlimentos_model.Trabajadores;
 import simulacionBancoAlimentos_service.BancoAlimento_servicio;
 
+// --- Clase ---
 public class GestionaBancoAlimentos {
 
+	// --- Atributos ---
 	private static final Logger logger = LogManager.getLogger(GestionaBancoAlimentos.class);
-	private static final String resource = "bancoAlimentos.xml";
-	private static final String rutaCSV = "src\\main\\resources\\voluntarios.csv";
+	private static final String resource = "bancoAlimentos.xml"; // solo el nombre del xml añadido en \\resources\\
+	private static final String rutaCSV = "src\\main\\resources\\voluntarios.csv"; // ruta completa donde quiero que se cree el archivo .csv
 
+	// --- Main ---
 	public static void main(String[] args) throws Exception {
 
-		BancoAlimento_servicio servicio = new BancoAlimento_servicio();
+		BancoAlimento_servicio servicio = new BancoAlimento_servicio(); // objeto servicio
 
-		List<Trabajadores> list = servicio.leerTrabajadoresListDesdeXML(resource);
+		// lista con los datos de los trabajadores
+		List<Trabajadores> list = servicio.leerTrabajadoresListDesdeXML(resource); 
+		// lista con los datos de los centros logisticos
 		List<CentroLogistico> list2 = servicio.leerCentroListDesdeXML(resource);
 
-
+		// llamadas a los métodos 
 		try {
-			logger.info(servicio.leerCentroListDesdeXML(resource));
+			logger.info(servicio.leerCentroListDesdeXML(resource)); 
 		} catch (Exception e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
