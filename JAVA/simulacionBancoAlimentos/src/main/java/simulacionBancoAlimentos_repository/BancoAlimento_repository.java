e808952b2e17605d311be258c40c099b86a48cac
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-13 20:51:33 +0100

    Add comments to bancoAlimento_repository

--- JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_repository/BancoAlimento_repository.java
+++ JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_repository/BancoAlimento_repository.java
@@ -1,5 +1,6 @@
 package simulacionBancoAlimentos_repository;
 
+// --- Imports ---
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -11,12 +12,15 @@ import simulacionBancoAlimentos_model.BancoException;
 import simulacionBancoAlimentos_model.CentroLogistico;
 import simulacionBancoAlimentos_model.Trabajadores;
 
+// --- Clase ---
 public class BancoAlimento_repository {
 
+	// --- Atributos ---
 	private static final Logger logger = LogManager.getLogger(BancoAlimento_repository.class);
-	private List<CentroLogistico> centroLogisticos;
-	private List<Trabajadores> trabajadores;
+	private List<CentroLogistico> centroLogisticos; // Lista centros logísticos.
+	private List<Trabajadores> trabajadores; // Lista trabajadores
 
+	// --- Getters y setters --- 
 	public List<Trabajadores> getTrabajadores() {
 		return trabajadores;
 	}
@@ -37,57 +41,69 @@ public class BancoAlimento_repository {
 		return logger;
 	}
 
+	// --- toString ---
 	@Override
 	public String toString() {
 		return "BancoAlimento_repository [centroLogisticos=" + centroLogisticos + ", trabajadores=" + trabajadores
 				+ "]";
 	}
 
+	// --- Constructor sin parámetros ---
 	public BancoAlimento_repository() {
 		super();
-		this.centroLogisticos = new ArrayList<CentroLogistico>();
-		this.trabajadores = new ArrayList<Trabajadores>();
+		this.centroLogisticos = new ArrayList<CentroLogistico>(); // Inicializa lista centros logísticos (!= null)
+		this.trabajadores = new ArrayList<Trabajadores>(); // Inicializa lista trabajadores (!= null)
 	}
 
+	// --- Método de añadir centro logístico en la lista creada anteriormente ---
 	public void añadirCentro(CentroLogistico centroLogistico) throws BancoException {
+		// si centroLogisticos contiene el 'id' del centro pasado por parmetro...
 		if (centroLogisticos.contains(centroLogistico.getId())) {
 			throw new BancoException("Ese id ya existe en el centro logístico.");
 		}
-		centroLogisticos.add(centroLogistico);
+		centroLogisticos.add(centroLogistico); // añade centro a la lista
 	}
 
+	// --- Método de añadir trabajador a centro logístico --- 
 	public void añadirTrabajadorACentro(CentroLogistico centroLogistico, Trabajadores trabajadores)
 			throws BancoException {
+		// si la lista de trabajadores del centro pasado por parámetro contiene al trabajador pasado por parámetro...
 		if (centroLogistico.getListaTrabajadores().contains(trabajadores)) {
 			throw new BancoException("Ese trabajador ya existe en el centro logístico.");
 		}
-		centroLogistico.getListaTrabajadores().add(trabajadores);
+		centroLogistico.getListaTrabajadores().add(trabajadores); // añade trabajador a lista de trabajadores del centro
 	}
 
+	// --- Método de leer centro por id---
 	public CentroLogistico leerCentro(String id) throws BancoException {
-		CentroLogistico centro1 = null;
-		boolean encontrado = false;
-		Iterator<CentroLogistico> c = centroLogisticos.iterator();
+		CentroLogistico centro1 = null; // inicializa centro1 en null
+		boolean encontrado = false; // crea booleano en false
+		Iterator<CentroLogistico> c = centroLogisticos.iterator(); // intera centros
+		// mientras que 'encontrado' = true y haya más elementos en 'c'...
 		while (!encontrado && c.hasNext()) {
+			// crea objeto centro2 que contiene los 'c' leidos
 			CentroLogistico centro2 = c.next();
 			if (centro2.getId().equals(id)) {
-				centro1 = centro2;
+				centro1 = centro2; // (centro1=null) ahora contiene centro2
 				encontrado = true;
 			} else {
 				throw new BancoException("Este centro ya existe.");
 			}
 		}
-		return centro1;
+		return centro1; 
 	}
-	
+
+	// --- Método de leer trabajador por dni---
 	public Trabajadores leerTrabajador(String dni) throws BancoException {
-		Trabajadores trabajador1 = null;
-		boolean encontrado = false;
-		Iterator<Trabajadores> t = trabajadores.iterator();
+		Trabajadores trabajador1 = null; // inicializa trabajador1 en null
+		boolean encontrado = false; // crea booleano en false
+		Iterator<Trabajadores> t = trabajadores.iterator(); // intera trabajadores
+		// mientras que 'encontrado' = true y haya más elementos en 'c'...
 		while (!encontrado && t.hasNext()) {
+			// crea objeto trabajador2 que contiene los 'c' leidos
 			Trabajadores trabajador2 = t.next();
 			if (trabajador2.getDNI().equals(dni)) {
-				trabajador1 = trabajador2;
+				trabajador1 = trabajador2; // (trabajador1=null) ahora contiene trabajador2
 				encontrado = true;
 			} else {
 				throw new BancoException("Este trabajador ya existe.");
