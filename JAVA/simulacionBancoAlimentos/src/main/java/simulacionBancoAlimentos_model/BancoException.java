c6058d52f66c7a8b3388fe8fd0fc07766c2778cc
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-13 20:28:08 +0100

    Add commits to bancoException_model

--- JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_model/BancoException.java
+++ JAVA/simulacionBancoAlimentos/src/main/java/simulacionBancoAlimentos_model/BancoException.java
@@ -7,6 +7,7 @@ public class BancoException extends Exception {
 	 */
 	private static final long serialVersionUID = 1L;
 
+	// --- Contructor con mensaje ---
 	public BancoException(String mensaje) {
 		super(mensaje);
 	}s
