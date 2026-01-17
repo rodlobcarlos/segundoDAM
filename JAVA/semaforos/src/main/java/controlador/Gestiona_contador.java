549fcb05dc593371c8270438e4790b99690d01a1
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-17 18:51:50 +0100

    Create Gestiona_contador.java

---
+++ JAVA/semaforos/src/main/java/controlador/Gestiona_contador.java
@@ -0,0 +1,26 @@
+package controlador;
+
+import hilos.Contador;
+import hilos.TareaIncremento;
+
+public class Gestiona_contador {
+
+	public static void main(String[] args) throws InterruptedException {
+
+		Contador contador = new Contador();
+
+		// Cada hilo llega a mil 
+		Thread hilo1 = new Thread(new TareaIncremento(contador)); 
+		Thread hilo2 = new Thread(new TareaIncremento(contador));
+
+		// Cuando el hilo1 termina, empieza el hilo2 -> solucion = 2000
+		hilo1.start();
+		hilo1.join(); 
+		// join(): no se ejecuta otro hilo hasta que el actual termine
+		hilo2.start();
+		hilo2.join();
+
+		System.out.println("Valor final: " + contador.getValor());
+	} 
+
+}
