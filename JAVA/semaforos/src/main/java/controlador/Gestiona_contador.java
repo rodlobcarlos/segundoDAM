348277d29a9ac3e48dcff072cae82cae3b5f11e8
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-17 18:55:16 +0100

    Update Gestiona_contador.java

--- JAVA/semaforos/src/main/java/controlador/Gestiona_contador.java
+++ JAVA/semaforos/src/main/java/controlador/Gestiona_contador.java
@@ -13,11 +13,10 @@ public class Gestiona_contador {
 		Thread hilo1 = new Thread(new TareaIncremento(contador)); 
 		Thread hilo2 = new Thread(new TareaIncremento(contador));
 
-		// Cuando el hilo1 termina, empieza el hilo2 -> solucion = 2000
-		hilo1.start();
-		hilo1.join(); 
 		// join(): no se ejecuta otro hilo hasta que el actual termine
+		hilo1.start();
 		hilo2.start();
+		hilo1.join(); 
 		hilo2.join();
 
 		System.out.println("Valor final: " + contador.getValor());
