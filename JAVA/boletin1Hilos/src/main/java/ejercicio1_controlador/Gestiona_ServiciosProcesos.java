0fd3077d2916649dff217da29e835f4bd812f404
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-18 12:50:31 +0100

    Create Proceso.java

---
+++ JAVA/boletin1Hilos/src/main/java/ejercicio1_controlador/Gestiona_ServiciosProcesos.java
@@ -0,0 +1,20 @@
+package ejercicio1_controlador;
+
+import ejercicio1_modelo.Proceso;
+import ejercicio1_modelo.Servicio;
+
+public class Gestiona_ServiciosProcesos {
+
+	public static void main(String[] args) {
+
+		Proceso hilo1 = new Proceso();
+		Servicio hilo2 = new Servicio();
+		
+		// No se muestra de forma ordenada
+		hilo1.start();
+		hilo2.start();
+		
+		System.out.println("Fin del programa.");
+	}
+
+}
