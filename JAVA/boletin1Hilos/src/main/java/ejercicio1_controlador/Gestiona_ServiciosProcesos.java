fddeade90e912659f4f8b2e63af9efb2673bba1a
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-18 14:06:58 +0100

    Update Gestiona_ServiciosProcesos.java

--- JAVA/boletin1Hilos/src/main/java/ejercicio1_controlador/Gestiona_ServiciosProcesos.java
+++ JAVA/boletin1Hilos/src/main/java/ejercicio1_controlador/Gestiona_ServiciosProcesos.java
@@ -1,5 +1,7 @@
 package ejercicio1_controlador;
 
+import java.util.Scanner;
+
 import ejercicio1_modelo.Proceso;
 import ejercicio1_modelo.Servicio;
 
@@ -13,6 +15,13 @@ public class Gestiona_ServiciosProcesos {
 		// No se muestra de forma ordenada
 		hilo1.start();
 		hilo2.start();
+		
+		Scanner scanner = new Scanner(System.in);
+		scanner.nextLine(); // Salto de linea (enter)
+		
+		// Interrumpe el hilo que hemos manejado para que termine
+		hilo1.interrupt(); // Hilo interrumpido
+		hilo2.interrupt();
 		
 		System.out.println("Fin del programa.");
 	}
