dbe7fb75af4edfd4c279b6dfe0b29e6b0f7e8d22
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-19 13:32:53 +0100

    Update MainActivity.java

--- SegundoTrimestre/app/src/main/java/android/ejemplo/segundotrimestre/MainActivity.java
+++ SegundoTrimestre/app/src/main/java/android/ejemplo/segundotrimestre/MainActivity.java
@@ -2,6 +2,10 @@ package android.ejemplo.segundotrimestre;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.ContextMenu;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
@@ -11,6 +15,7 @@ import android.widget.ListView;
 import android.widget.RatingBar;
 import android.widget.SeekBar;
 import android.widget.Spinner;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,38 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i("menus",item.toString());
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuelemento, menu);
+    }
+
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        Log.i("menus",item.toString());
+        return super.onContextItemSelected(item);
+    }
+
+
+
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -31,99 +68,33 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
 
-        /**
-        String[] opciones = {"Ford mustang", "Maclaren", "Mercedes", "Toyota", "Mazda", "Honda", "Jeep", "Seat"};
-        AutoCompleteTextView textoLeido = (AutoCompleteTextView) findViewById(R.id.miTexto);
-        ArrayAdapter<String> adapador = new ArrayAdapter<String> (this, android.R.layout.simple_dropdown_item_1line, opciones);
-        textoLeido.setAdapter(adapador);
-**/
-
-        /**
-        SeekBar miControl = (SeekBar) findViewById(R.id.miSeekBar);
-        miControl.setOnSeekBarChangeListener(new SeekBar.OnSeekBarChangeListener() {
-            @Override
-            public void onProgressChanged(SeekBar seekBar, int progress, boolean fromUser) {
-                Log.i("MiBar","volumen:"+ seekBar.getProgress());
-            }
-            @Override
-            public void onStartTrackingTouch(SeekBar seekBar) {
-                Log.i("MiBar","volumen inicial:"+ seekBar.getProgress());
-            }
-            @Override
-            public void onStopTrackingTouch(SeekBar seekBar) {
-                Log.i("MiBar","volumen final:"+ seekBar.getProgress());
-            }
-        });
-         **/
-
-/**
-        RatingBar controlRating =  (RatingBar) findViewById(R.id.myRating);
-        controlRating.setOnRatingBarChangeListener(new RatingBar.OnRatingBarChangeListener() {
-            @Override
-            public void onRatingChanged(RatingBar ratingBar, float rating, boolean fromUser) {
-                Log.i("Rating","Valor de rating: "+rating);
-            }
-        });
- **/
-
-/**
-        ListView listado = (ListView) findViewById(R.id.miLista);
-        final String[] datos = new String[]{"Tulipan", "Margarita", "Margarita", "Rosa", "Clavel", "Gardenia", "Girasol","Jazmin"};
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,datos);
-        listado.setAdapter(adaptador);
-
-        listado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("Pulsado", "Flor pulsada (posicion): "+position);
-                Log.i("Pulsado", "Flor pulsada: "+(String) parent.getItemAtPosition(position));
-            }
-        });
-**/
-
-/**
-        GridView listado = (GridView) findViewById(R.id.miGrid);
-        final String[] datos = new String[]{"Casa", "Coche", "Garaje", "Mesa", "Jardin", "Piscina","Cesped","Fachada"};
-        ArrayAdapter<String> adaptador = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1,datos);
-        listado.setAdapter(adaptador);
-
-        listado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("Pulsado", "Elemento de casa pulsado (posicion): "+position);
-                Log.i("Pulsado", "Elemento de casa pulsado: "+(String) parent.getItemAtPosition(position));
-            }
-        });
-
-**/
-
-/**
-        Spinner listaSpinner = (Spinner) findViewById(R.id.miSpinner);
-        final String[] datosSpinner = new String[]{"LaLiga", "NBA", "NFL", "Hockey", "Golf"};
-        ArrayAdapter<String> adaptadorSpinner = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, datosSpinner);
-        listaSpinner.setAdapter(adaptadorSpinner);
-
-        listaSpinner.setOnItemSelectedListener(new AdapterView.OnItemSelectedListener(){
-            @Override
-            public void onItemSelected(AdapterView<?> parent, View view, int position, long id) {
-                Log.i("Pulsado", "Deporte pulsado (posicion): "+position);
-                Log.i("Pulsado", "Deporte pulsado: "+(String) parent.getItemAtPosition(position));
-            }
-            @Override
-            public void onNothingSelected(AdapterView<?> parent) {
-            }
-        });
-**/
-
+/*
+        TextView elemento = (TextView) findViewById(R.id.texto);
+        registerForContextMenu(elemento);
+*/
         Datos[] datos = new Datos[]{
-                new Datos("Campo", "field"),
-                new Datos("Coche", "Car"),
-                new Datos("Lapiz", "Pen"),
-                new Datos("Amor", "Love"),
-                new Datos("Piña", "Pineapple")  
+                new Datos("Linea superior 1", "Linea inferior 1"),
+                new Datos("Linea superior 2", "Linea inferior 2"),
+                new Datos("Linea superior 3", "Linea inferior 3"),
+                new Datos("Linea superior 4", "Linea inferior 4"),
+                new Datos("Linea superior 5", "Linea inferior 5")
         };
+
         ListView listado = (ListView) findViewById(R.id.miLista);
         Adaptador miAdaptador = new Adaptador(this, datos);
         listado.setAdapter(miAdaptador);
+
+        listado.setAdapter(miAdaptador);
+        registerForContextMenu(listado);
+
+        listado.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Log.i("Pulsado", "Elemento pulsado: "+position);
+                Log.i("Pulsado", "Elemento pulsado: "+(Datos) parent.getItemAtPosition(position));
+            }
+        });
+
+
     }
 }
