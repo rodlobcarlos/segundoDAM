5eff6677435b7a07488e8bbc28c7cf4f774a4e98
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-20 12:36:51 +0100

    Create Empleado.java

---
+++ JAVA/hibernate/src/main/java/hibernate_model/Empleado.java
@@ -0,0 +1,74 @@
+package hibernate_model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "empleado")
+public class Empleado {
+
+	@Id
+	@GeneratedValue(strategy = GenerationType.AUTO)
+	private int idEmpleado;
+
+	@Column(name = "nombre")
+	private String nombre;
+
+	@Column(name = "salario")
+	private double salario;
+
+	@OneToMany()
+	@JoinColumn(name = "idDepartamento")
+	private Departamento departamento;
+
+	public Empleado(String nombre, double salario) {
+		super();
+		this.nombre = nombre;
+		this.salario = salario;
+	}
+
+	public int getIdEmpleado() {
+		return idEmpleado;
+	}
+
+	public void setIdEmpleado(int idEmpleado) {
+		this.idEmpleado = idEmpleado;
+	}
+
+	public String getNombre() {
+		return nombre;
+	}
+
+	public void setNombre(String nombre) {
+		this.nombre = nombre;
+	}
+
+	public double getSalario() {
+		return salario;
+	}
+
+	public void setSalario(double salario) {
+		this.salario = salario;
+	}
+
+	public Departamento getDepartamentos() {
+		return departamento;
+	}
+
+	public void setDepartamentos(Departamento departamento) {
+		this.departamento = departamento;
+	}
+
+	@Override
+	public String toString() {
+		return "Empleado [idEmpleado=" + idEmpleado + ", nombre=" + nombre + ", salario=" + salario + ", departamento="
+				+ departamento + "]";
+	}
+
+}
