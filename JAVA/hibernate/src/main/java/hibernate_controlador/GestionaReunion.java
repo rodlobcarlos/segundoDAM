461f126aa73055749fea21dd19b80fca4c254698
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-20 18:33:19 +0100

    Update GestionaReunion.java

--- JAVA/hibernate/src/main/java/hibernate_controlador/GestionaReunion.java
+++ JAVA/hibernate/src/main/java/hibernate_controlador/GestionaReunion.java
@@ -4,10 +4,12 @@ import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.List;
 
+import hibernate_model.Acta;
 import hibernate_model.Departamento;
 import hibernate_model.Empleado;
 import hibernate_model.Reunion;
 import hibernate_model.Sala;
+import hibernate_repositorio.ActaDao;
 import hibernate_repositorio.DepartamentoDao;
 import hibernate_repositorio.EmpleadoDao;
 import hibernate_repositorio.ReunionDao;
@@ -35,20 +37,31 @@ public class GestionaReunion {
 			System.out.println(r);
 		}
 
+		Acta acta = new Acta("Acta reunion", reunion);
+		ActaDao actaDao = new ActaDao();
+		reunion.setActa(acta);
+//		actaDao.create(acta);
+
+		List<Acta> actas = actaDao.getAll();
+		for (Acta a : actas) {
+			System.out.println(a);
+		}
+
 		Departamento departamento = new Departamento("Departamento de tecnlogía");
 		DepartamentoDao departamentoDao = new DepartamentoDao();
 		departamentoDao.create(departamento);
 
-		List<Departamento> departamentos = new ArrayList<Departamento>();
+		List<Departamento> departamentos = departamentoDao.getAll();
 		for (Departamento d : departamentos) {
 			System.out.println(d);
 		}
 
 		Empleado empleado = new Empleado("Carlos", 1200);
+		empleado.setDepartamentos(departamento);
 		EmpleadoDao empleadoDao = new EmpleadoDao();
 		empleadoDao.create(empleado);
 
-		List<Empleado> empleados = new ArrayList<Empleado>();
+		List<Empleado> empleados = empleadoDao.getAll();
 		for (Empleado e : empleados) {
 			System.out.println(e);
 		}
