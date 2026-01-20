2979dd23730e7a20975bc8252a7f6e1a533f4c42
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-20 12:36:57 +0100

    Update Reunion.java

--- JAVA/hibernate/src/main/java/hibernate_model/Reunion.java
+++ JAVA/hibernate/src/main/java/hibernate_model/Reunion.java
@@ -6,6 +6,9 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 
 @Entity
@@ -20,5 +23,58 @@ public class Reunion {
 	private LocalDateTime fecha;
 	// @Column(name="asunto")
 	private String asunto;
+
+	@OneToOne()
+	@JoinColumn(name = "idSala")
+	private Sala sala;
+
 	// Generamos el constructor sin parámetros y los métodos get/set
+	public int getIdReunion() {
+		return idReunion;
+	}
+
+	public Reunion(LocalDateTime fecha, String asunto, Sala sala) {
+		super();
+		this.fecha = fecha;
+		this.asunto = asunto;
+		this.sala = sala;
+	}
+
+	public LocalDateTime getFecha() {
+		return fecha;
+	}
+
+	public void setFecha(LocalDateTime fecha) {
+		this.fecha = fecha;
+	}
+
+	public String getAsunto() {
+		return asunto;
+	}
+
+	public void setAsunto(String asunto) {
+		this.asunto = asunto;
+	}
+
+	public Sala getSala() {
+		return sala;
+	}
+
+	public void setSala(Sala sala) {
+		this.sala = sala;
+	}
+
+	public void setIdReunion(int idReunion) {
+		this.idReunion = idReunion;
+	}
+
+	@Override
+	public String toString() {
+		return "Reunion [idReunion=" + idReunion + ", fecha=" + fecha + ", asunto=" + asunto + "]";
+	}
+
+	public Reunion() {
+		super();
+	}
+
 }
