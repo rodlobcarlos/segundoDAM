ac793c32eb6f1c48b71fdf1c58659fd1088e019e
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-25 11:28:09 +0100

    package name -> changed

--- JAVA/comunicacionesRed/src/main/java/ejemploConexion/Cliente.java
+++ JAVA/comunicacionesRed/src/main/java/ejemploConexion/Cliente.java
@@ -1,4 +1,4 @@
-package conexion;
+package ejemploConexion;
 
 import java.io.IOException;
 
--- JAVA/comunicacionesRed/src/main/java/ejemploConexion/ClienteTCPUnMensaje.java
+++ JAVA/comunicacionesRed/src/main/java/ejemploConexion/ClienteTCPUnMensaje.java
@@ -1,4 +1,4 @@
-package conexion;
+package ejemploConexion;
 
 import java.io.BufferedReader;
 import java.io.IOException;
