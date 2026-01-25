ac42ee30b45b902dcbfced8e241e2f72b054eb19
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-25 11:18:39 +0100

    Update Cliente.java

--- JAVA/comunicacionesRed/src/main/java/conexion/Cliente.java
+++ JAVA/comunicacionesRed/src/main/java/conexion/Cliente.java
@@ -1,9 +1,8 @@
 package conexion;
 
-import java.io.BufferedReader;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.io.PrintWriter;
+
+import java.net.InetAddress;
 import java.net.Socket;
 import java.net.UnknownHostException;
 
@@ -11,31 +10,16 @@ public class Cliente {
 	public static void main(String[] args) {
 		String Host = "localhost"; // host servidor con el que el cliente quiere conectarse
 		int Puerto = 6000;// puerto remoto en el servidor que el cliente conoce
-		Socket cliente;
+		Socket cliente = null;
 		try {
 			cliente = new Socket(Host, Puerto);
-			// conecta
-//			InetAddress i = cliente.getInetAddress();
-//			System.out.println("Puerto local: " + cliente.getLocalPort());
-//			System.out.println("Puerto Remoto: " + cliente.getPort());
-//			System.out.println("Host Remoto: " + i.getHostName().toString());
-//			System.out.println("IP Host Remoto: " + i.getHostAddress().toString());
 
-			// Configura flujos para enviar y recibir datos
-			PrintWriter salida = new PrintWriter(cliente.getOutputStream(), true);
-			BufferedReader entrada = new BufferedReader(new InputStreamReader(cliente.getInputStream()));
-			String mensajeParaServidor = "Hola soy un cliente";
-
-			// Enviar mensaje al servidor
-			salida.println(mensajeParaServidor);
-			System.out.println("Mensaje enviado. Esperando respuesta del servidor...");
-
-			String repuestaServidor = entrada.readLine();
-			while (repuestaServidor != null) {
-				if (repuestaServidor == "fin") {
-					System.out.println("Respuesta del servidor: " + repuestaServidor);
-				}
-			}
+			// informativo
+			InetAddress i = cliente.getInetAddress();
+			System.out.println("Cliente : Puerto local: " + cliente.getLocalPort());
+			System.out.println("Cliente : Puerto Remoto: " + cliente.getPort());
+			System.out.println("Cliente : Host Remoto: " + i.getHostName());
+			System.out.println("Cliente : IP Host Remoto: " + i.getHostAddress());
 		} catch (UnknownHostException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
