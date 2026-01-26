39d5745a277e8de69f0edc43282ed642b57c05fd
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-26 18:19:06 +0100

    Update Servidor.java

--- JAVA/comunicacionesRed/src/main/java/boletin1_ejercicio2/Servidor.java
+++ JAVA/comunicacionesRed/src/main/java/boletin1_ejercicio2/Servidor.java
@@ -9,11 +9,11 @@ public class Servidor {
 	public static void main(String[] args) {
 		int puerto = 4444;
 		try (ServerSocket servidor = new ServerSocket(puerto)) {
-			System.out.println("Servidor multihilo iniciado en el puerto " + puerto);
+			System.out.println("Servidor iniciado...");
 			while (true) {
 				// 1. Espera a un cliente
 				Socket socketCliente = servidor.accept();
-				System.out.println("Nuevo cliente conectado: " + socketCliente.getInetAddress() + " " + LocalDateTime.now());
+				System.out.println("Conecta IP " + socketCliente.getInetAddress() + ", Puerto remoto: " + socketCliente.getPort());
 				// 2. Lanza un hilo nuevo para este cliente específico
 				// Esto permite que el bucle vuelva al accept() inmediatamente
 				new Servidor_hilo(socketCliente).start();
