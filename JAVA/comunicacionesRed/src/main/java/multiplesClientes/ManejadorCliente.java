7bc6a602a5c700e2dab9d3565a41db5f4b3bf931
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-26 11:41:15 +0100

    Create ManejadorCliente.java

---
+++ JAVA/comunicacionesRed/src/main/java/multiplesClientes/ManejadorCliente.java
@@ -0,0 +1,35 @@
+package multiplesClientes;
+
+import java.io.BufferedReader;
+
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+import boletin1_ejercicio3.Contador;
+
+public class ManejadorCliente {
+
+	public static void main(String[] args) {
+		String host = "localhost";
+		int puerto = 4444; //
+
+		try (Socket socket = new Socket(host, puerto); // Conexión
+				PrintWriter salida = new PrintWriter(socket.getOutputStream(), true); //
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream())); //
+				Scanner sc = new Scanner(System.in)) {
+
+			System.out.println("Cliente : Conectado al servidor.");
+
+			// Leemos el saludo inicial del servidor multihilo
+			String saludo = entrada.readLine();
+			System.out.println("Servidor dice: " + saludo);
+
+		} catch (IOException e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+		System.out.println("Cliente : Conexión cerrada.");
+	}
+}
