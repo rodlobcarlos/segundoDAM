b5c22e6689119ca61df2537becd13ceddf2100e5
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-26 18:18:57 +0100

    Update ClienteHilo.java

--- JAVA/comunicacionesRed/src/main/java/boletin1_ejercicio2/ClienteHilo.java
+++ JAVA/comunicacionesRed/src/main/java/boletin1_ejercicio2/ClienteHilo.java
@@ -8,31 +8,34 @@ import java.io.PrintWriter;
 import java.net.Socket;
 import java.util.Scanner;
 
-public class ClienteHilo {
-	public static void main(String[] args) {
-		String host = "localhost";
-		int puerto = 4444; //
+public class ClienteHilo extends Thread {
+//	public static void main(String[] args) {
+	String host = "localhost";
+	int puerto = 4444; //
 
+	@Override
+	public void run() {
 		try (Socket socket = new Socket(host, puerto); // Conexión
-				PrintWriter salida = new PrintWriter(socket.getOutputStream(), true); //
-				BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream())); //
-				Scanner sc = new Scanner(System.in)) {
+				PrintWriter salida = new PrintWriter(socket.getOutputStream(), true);
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream()));
 
-			String texto = "";
-			while (true) {
-				System.out.println("Escribe algo: ");
-				texto = sc.nextLine();
-				
-				salida.println(texto);
-				
-				// Leemos el saludo inicial del servidor multihilo
-				String saludo = entrada.readLine();
-				System.out.println("Servidor dice: " + saludo);
-			}
+				Scanner sc = new Scanner(System.in)) {
+			/*
+			 * String texto = "";
+			 * 
+			 * 
+			 * while (true) { System.out.println("Escribe algo: "); texto = sc.nextLine();
+			 * 
+			 * salida.println(texto);
+			 * 
+			 * // Leemos el saludo inicial del servidor multihilo String saludo =
+			 * entrada.readLine(); System.out.println("Servidor dice: " + saludo); }
+			 */
 
 		} catch (IOException e) {
 			System.err.println("Error: " + e.getMessage());
 		}
 		System.out.println("Cliente : Conexión cerrada.");
 	}
+
 }
