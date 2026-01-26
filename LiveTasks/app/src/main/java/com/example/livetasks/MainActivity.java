d600571b60381b0562ebd38957d6452f33309d64
rodlobcarlos <rodlobcarlos@gmail.com>
2026-01-26 21:57:17 +0100

    Update MainActivity.java

--- LiveTasks/app/src/main/java/com/example/livetasks/MainActivity.java
+++ LiveTasks/app/src/main/java/com/example/livetasks/MainActivity.java
@@ -1,6 +1,14 @@
 package com.example.livetasks;
 
 import android.os.Bundle;
+import android.util.Log;
+import android.view.ContextMenu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ListView;
+import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -8,7 +16,26 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
+import java.util.ArrayList;
+
 public class MainActivity extends AppCompatActivity {
+
+    @Override
+    public void onCreateContextMenu(ContextMenu menu, View v,
+                                    ContextMenu.ContextMenuInfo menuInfo) {
+        super.onCreateContextMenu(menu, v, menuInfo);
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menuelemento, menu);
+    }
+
+    // Capturamos las opciones
+    @Override
+    public boolean onContextItemSelected(MenuItem item) {
+        AdapterView.AdapterContextMenuInfo info = (AdapterView.AdapterContextMenuInfo) item.getMenuInfo();
+        Log.i("menus",item.toString());
+        return super.onContextItemSelected(item);
+    }
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -20,5 +47,15 @@ public class MainActivity extends AppCompatActivity {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
         });
+
+        ArrayList<Datos> datos = new ArrayList<Datos>();
+        datos.add(new Datos("Linea superior 1", "Linea inferior 1"));
+        datos.add(new Datos("Linea superior 2", "Linea inferior 2"));
+        datos.add(new Datos("Linea superior 3", "Linea inferior 3"));
+
+        ListView listado = (ListView) findViewById(R.id.miLista);
+        Adaptador miAdaptador = new Adaptador(this, datos);
+        listado.setAdapter(miAdaptador);
+
     }
 }
