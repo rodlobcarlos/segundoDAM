a962c96f69ae82929fbbaf23935a78f3c018beb5
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-01-30 13:31:29 +0100

    changes

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/PlanActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/PlanActivity.java
@@ -1,8 +1,6 @@
 package com.example.livetasks;
 
-import android.graphics.Color;
 import android.os.Bundle;
-import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,14 +8,12 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class MainActivity extends AppCompatActivity {
+public class PlanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_main);
-        TextView miTexto= (TextView) findViewById(R.id.titulo);
-        miTexto.setTextColor(Color.BLACK);
+        setContentView(R.layout.activity_plan);
     };
 }
