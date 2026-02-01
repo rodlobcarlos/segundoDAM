9a5317995a9669bd3359f24ee7d236b5b8b298f3
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-02-01 19:43:23 +0100

    exercice1 repasoRed -> done

---
+++ JAVA/comunicacionesRed/src/main/java/repasoRed_Ejercicio1/Cliente.java
@@ -0,0 +1,37 @@
+package repasoRed_Ejercicio1;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
+import java.net.Socket;
+import java.util.Scanner;
+
+public class Cliente {
+
+	public static void main(String[] args) {
+
+		String host = "localhost";
+		int puerto = 4444; //
+
+		try (Socket socket = new Socket(host, puerto); // Conexión
+				PrintWriter salida = new PrintWriter(socket.getOutputStream(), true); //
+				BufferedReader entrada = new BufferedReader(new InputStreamReader(socket.getInputStream())); //
+				Scanner sc = new Scanner(System.in)) {
+
+			System.out.println("Cliente : Conectado al servidor.");
+			System.out.println("Escribe un mensaje para el servidor: ");
+			String mensaje = sc.nextLine();
+			
+			salida.println(mensaje);
+
+			String repuestaServidor = entrada.readLine();
+			System.out.println(repuestaServidor);
+
+		} catch (IOException e) {
+			System.err.println("Error: " + e.getMessage());
+		}
+		System.out.println("Cliente : Conexión cerrada.");
+	}
+
+}
