b2405cb7ad35c11760032a92d24c5f5ea162b5a0
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-02-05 09:54:51 +0100

    Create operaciones.py

---
+++ desarrolloInterfaces/tema5/pytest/operaciones.py
@@ -0,0 +1,6 @@
+def suma_enteros(sumando1, sumando2):
+    if type(sumando1) != int or type(sumando2) != int:
+        resultado = None
+    else:
+        resultado = sumando1 + sumando2
+    return resultado
