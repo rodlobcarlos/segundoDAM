6b81aa428cf271b62aa37355bd7d58c1b8a544ea
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-02-13 11:32:08 +0100

    Create CalendarioActivity.java

---
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/CalendarioActivity.java
@@ -0,0 +1,31 @@
+package com.example.livetasks;
+
+import android.os.Bundle;
+import android.widget.CalendarView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class CalendarioActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_calendario);
+
+        CalendarView calendarView = findViewById(R.id.calendarView);
+
+        calendarView.setOnDateChangeListener(new CalendarView.OnDateChangeListener() {
+            @Override
+            public void onSelectedDayChange(@NonNull CalendarView view, int year, int month, int dayOfMonth) {
+                // El mes (month) empieza en 0 (Enero es 0, Diciembre es 11)
+                String fechaSeleccionada = dayOfMonth + "/" + (month + 1) + "/" + year;
+            }
+        });
+    }
+}
