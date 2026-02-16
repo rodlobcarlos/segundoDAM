633d409b6f1aa6de35a42ce0ea8423fb11af10cc
Carlos Rodríguez Lobato <rodlobcarlos@gmail.com>
2026-02-16 11:58:26 +0100

    project deleted

--- Proyecto-transversal/JuegosOlimpicos/src/main/resources/creacionBD.sql
+++
@@ -1,8 +0,0 @@
-create database proyectoTransversal;
-use proyectoTransversal;
-
-CREATE USER IF NOT EXISTS 'carlos'@'%' IDENTIFIED BY 'clave';
---  Asignar privilegios
-GRANT ALL PRIVILEGES ON `proyectoTransversal`.* TO 'carlos'@'%';
---  Refrescar
-FLUSH PRIVILEGES;
