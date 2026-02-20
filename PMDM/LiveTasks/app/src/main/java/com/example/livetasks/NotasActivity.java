9f7cd2b649a7796314e872e84164a73e44e493c5
rodlobcarlos <rodlobcarlos@gmail.com>
2026-02-20 21:56:31 +0100

    Update NotasActivity.java

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/NotasActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/NotasActivity.java
@@ -2,8 +2,12 @@ package com.example.livetasks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -13,11 +17,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class NotasActivity extends AppCompatActivity {
 
+    private ScrollView scrollContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_notas);
+
+        scrollContent = findViewById(R.id.notas_scroll);
+
+        scrollContent.post(() -> scrollContent.fullScroll(ScrollView.FOCUS_UP));
+
+        // 1. Buscamos la barra que pusimos en el XML
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar_tareas);
+
+        // 2. Le decimos a la App que esta es la "ActionBar" oficial
+        setSupportActionBar(toolbar);
 
         Button btnTarea = findViewById(R.id.btn_tareas);
         btnTarea.setOnClickListener(new View.OnClickListener() {
@@ -45,5 +61,16 @@ public class NotasActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i("menus",item.toString());
+        return super.onOptionsItemSelected(item);
     }
 }
