c0bf512120b2069a0b327ad29a8f4b57f9c5747d
rodlobcarlos <rodlobcarlos@gmail.com>
2026-02-20 21:56:25 +0100

    Update CalendarioActivity.java

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/CalendarioActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/CalendarioActivity.java
@@ -2,9 +2,13 @@ package com.example.livetasks;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.CalendarView;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.annotation.NonNull;
@@ -15,11 +19,23 @@ import androidx.core.view.WindowInsetsCompat;
 
 public class CalendarioActivity extends AppCompatActivity {
 
+    private ScrollView scrollContent;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_calendario);
+
+        scrollContent = findViewById(R.id.scroll_content);
+
+        scrollContent.post(() -> scrollContent.fullScroll(ScrollView.FOCUS_UP));
+
+        // 1. Buscamos la barra que pusimos en el XML
+        androidx.appcompat.widget.Toolbar toolbar = findViewById(R.id.toolbar_tareas);
+
+        // 2. Le decimos a la App que esta es la "ActionBar" oficial
+        setSupportActionBar(toolbar);
 
         CalendarView calendarView = findViewById(R.id.calendarView);
 
@@ -53,10 +69,20 @@ public class CalendarioActivity extends AppCompatActivity {
         btnNotas.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(CalendarioActivity.this, MonitoreoActivity.class);
+                Intent intent = new Intent(CalendarioActivity.this, NotasActivity.class);
                 startActivity(intent);
             }
         });
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.mainmenu, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        Log.i("menus",item.toString());
+        return super.onOptionsItemSelected(item);
     }
 }
