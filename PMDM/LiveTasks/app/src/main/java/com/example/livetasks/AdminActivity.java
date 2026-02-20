b3cfdff9157a315c9d492f012ebf2927e2b72e37
rodlobcarlos <rodlobcarlos@gmail.com>
2026-02-20 21:56:22 +0100

    Update AdminActivity.java

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/AdminActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/AdminActivity.java
@@ -15,6 +15,5 @@ public class AdminActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
-
     }
 }
