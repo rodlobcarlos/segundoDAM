e0cff9e7b6c2ea3311dce0bd6ff99f6617bd0904
rodlobcarlos <rodlobcarlos@gmail.com>
2026-02-21 13:44:42 +0100

    Update AdminActivity.java

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/AdminActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/AdminActivity.java
@@ -1,6 +1,7 @@
 package com.example.livetasks;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -9,11 +10,16 @@ import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
 public class AdminActivity extends AppCompatActivity {
+    private ScrollView scrollContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
         setContentView(R.layout.activity_admin);
+
+        scrollContent = findViewById(R.id.scroll_admin);
+
+        scrollContent.post(() -> scrollContent.fullScroll(ScrollView.FOCUS_UP));
     }
 }
