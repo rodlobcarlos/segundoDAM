71c4e069ba4ca343c8c388ae2067348bf2a68276
rodlobcarlos <rodlobcarlos@gmail.com>
2026-02-22 11:19:06 +0100

    Update CuentaActivity.java

--- PMDM/LiveTasks/app/src/main/java/com/example/livetasks/CuentaActivity.java
+++ PMDM/LiveTasks/app/src/main/java/com/example/livetasks/CuentaActivity.java
@@ -1,11 +1,14 @@
 package com.example.livetasks;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
@@ -27,5 +30,36 @@ public class CuentaActivity extends AppCompatActivity {
                 startActivity(intent);
             }
         });
+
+        Button btnSesion = findViewById(R.id.cerrar_sesion);
+        btnSesion.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                mostrarDialogoConfirmacion();
+            }
+        });
+    }
+
+    private void mostrarDialogoConfirmacion() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("¿Quieres cerrar sesión?")
+                .setTitle("Cerrar sesión")
+                .setIcon(R.mipmap.ic_launcher)
+                .setPositiveButton("Aceptar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Elegido", "Cierra sesión.");
+                        Intent intent = new Intent(CuentaActivity.this, LoginActivity.class);
+                        startActivity(intent);
+                    }
+                })
+                .setNegativeButton("Rechazar", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialog, int which) {
+                        Log.i("Elegido", "El usuario prefiere no cerrar sesión.");
+                    }
+                });
+        AlertDialog dialog = builder.create();
+        dialog.show();
     }
 }
